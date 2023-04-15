2d722ce5667b6ff1077f6aa1a2b774bae6574f7e
0400-Rashmi <daha6801@stthomas.edu>
2023-04-15 14:49:03 -0500

    Adding a utility project to apply the database migrations

---
+++ WebApplication1/WebApplication1.Migrations/ConsoleStartup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Migrations
+{
+    public class ConsoleStartup
+    {
+        public ConsoleStartup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ArtProductAppCon"));
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1.Migrations/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Migrations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Applying migrations");
+        var webHost = new WebHostBuilder()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseStartup<ConsoleStartup>()
+            .Build();
+        using (var context = (AppDbContext)webHost.Services.GetService(typeof(AppDbContext)))
+        {
+            context.Database.Migrate();
+        }
+        Console.WriteLine("Done");
+    }
+}
